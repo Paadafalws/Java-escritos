78f11fb43c81cfcbb17c0dd4062a5e88caae7a59
Paadafalws <paul.lockeex@gmail.com>
2022-09-18 02:17:21 -0300

    e

--- passo a passo/metoths/array + for.js
+++ passo a passo/metoths/array + for.js
@@ -12,3 +12,4 @@ while (!isOver) {
 for (let i = 0; i < names.length; i++){
     console.log(names[i]);
 }
+
---
+++ passo a passo/metoths/metoodo.js
@@ -0,0 +1,8 @@
+function showMsg() {
+    console.log("se o metodo tiver return; vai voltar ");
+        return;//retun indica o fim do retorno 
+    console.log("se o meotodo nao tiver vai voltar esse ");
+
+}
+
+showMsg(); // -> message 1
